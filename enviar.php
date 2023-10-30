8525023556f5932e52e148560e0b0fe9b1c2f748
NoeValenzuela <149193142+NoeValenzuela@users.noreply.github.com>
2023-10-29 22:01:06 -0700

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php 
+            $nombre = $_POST['nombre'];
+            $email = $_POST['email'];
+            $telefono = $_POST['telefono'];
+            $asunto = 'Formulario Rellenado';
+            $mensaje = "Nombre: ".$nombre."<br> Email: $email<br> Telefono: $telefono<br> Mensaje:".$_POST['mensaje'];
+            
+            if(mail('tuEmail', $asunto, $mensaje)){
+                echo "Correo enviado";
+            }
+        ?>
+    </body>
+</html>
